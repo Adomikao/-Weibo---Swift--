f87c79cc1cfe4e2676130b09a84ec0a5c74a2909
Adomikao <Adomikao@foxmail.com>
2016-08-28 09:47:09 +0800

    项目初始化
    1.配置项目基本信息
    2.创建项目的类文件

---
+++ 仿Weibo客户端（Swift版）/仿Weibo客户端（Swift版）/Classes/Main/JYMainController.swift
@@ -0,0 +1,35 @@
+//
+//  JYMainController.swift
+//  仿Weibo客户端（Swift版）
+//
+//  Created by yongjianyu on 16/8/28.
+//  Copyright © 2016年 Adomikao Inc. All rights reserved.
+//
+
+import UIKit
+
+class JYMainController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ 仿Weibo客户端（Swift版）/仿Weibo客户端（Swift版）Tests/_Weibo____Swift__Tests.swift
@@ -0,0 +1,36 @@
+//
+//  _Weibo____Swift__Tests.swift
+//  仿Weibo客户端（Swift版）Tests
+//
+//  Created by yongjianyu on 16/8/26.
+//  Copyright © 2016年 Adomikao Inc. All rights reserved.
+//
+
+import XCTest
+@testable import _Weibo____Swift__
+
+class _Weibo____Swift__Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
