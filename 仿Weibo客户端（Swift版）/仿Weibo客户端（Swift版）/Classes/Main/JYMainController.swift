99e8b90056932ae2ccaf5f446f4d842eec407a7d
Adomikao <Adomikao@foxmail.com>
2016-08-28 11:49:11 +0800

    add publish button
    添加发布加号按钮

--- 仿Weibo客户端（Swift版）/仿Weibo客户端（Swift版）/Classes/Main/JYMainController.swift
+++ 仿Weibo客户端（Swift版）/仿Weibo客户端（Swift版）/Classes/Main/JYMainController.swift
@@ -18,9 +18,19 @@ class JYMainController: UITabBarController {
         // 2.添加子控制器
         addChildController(JYHomeViewController(), title: "首页", imageName: "tabbar_home")
         addChildController(JYMessageViewController(), title: "消息", imageName: "tabbar_message_center")
+        // 再添加一个占位控制器
+        addChildViewController(JYNilViewController())
+        
         addChildController(JYDiscoverViewController(), title: "广场", imageName: "tabbar_discover")
         addChildController(JYProfileViewController(), title: "我", imageName: "tabbar_profile")
         
+        // 3.添加加号发布按钮
+        setupPublishBtn()
+        
+    }
+    
+    func publishClick(){
+        print(#function)
     }
     
     // 初始化子控制器
@@ -37,6 +47,39 @@ class JYMainController: UITabBarController {
         // 3.
         addChildViewController(nav)
         
+    }
+    
+    // 加号发布按钮
+    private lazy var publishBtn:UIButton = {
+        let btn = UIButton()
+        
+        // 设置前景图片
+        btn.setImage(UIImage(named:"tabbar_compose_icon_add"), forState: UIControlState.Normal)
+        btn.setImage(UIImage(named:"tabbar_compose_icon_add_highlighted"), forState: UIControlState.Highlighted)
+        
+        // 设置背景图片
+        btn.setBackgroundImage(UIImage(named:"tabbar_compose_button"), forState: UIControlState.Normal)
+        btn.setBackgroundImage(UIImage(named:"tabbar_compose_button_highlighted"), forState: UIControlState.Highlighted)
+        
+        // 监听发布按钮点击
+        btn.addTarget(self, action: #selector(JYMainController.publishClick), forControlEvents: UIControlEvents.TouchUpInside)
+        
+        
+        return btn
+    
+    }()
+    
+    private func setupPublishBtn(){
+        // 1.添加加号发布按钮
+        tabBar.addSubview(publishBtn)
+        
+        // 2.设置按钮的位置
+        let width = UIScreen.mainScreen().bounds.size.width / CGFloat(viewControllers!.count)
+        let rect = CGRect(x: 0, y: 0, width: width, height: 49)
+        
+        publishBtn.frame = CGRectOffset(rect, width * 2, 0)
+    
+    
     }
 
 }
---
+++ 仿Weibo客户端（Swift版）/仿Weibo客户端（Swift版）/Classes/Tools/JYNilViewController.swift
@@ -0,0 +1,13 @@
+//
+//  JYNilViewController.swift
+//  仿Weibo客户端（Swift版）
+//
+//  Created by yongjianyu on 16/8/28.
+//  Copyright © 2016年 Adomikao Inc. All rights reserved.
+//
+
+import UIKit
+
+class JYNilViewController: UIViewController{
+
+}
